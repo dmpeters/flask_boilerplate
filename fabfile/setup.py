36ad665d5e817c97fc8716c0e4e008ccb15ae2b4
dmpeters <dmpeters63@gmail.com>
2012-09-03 16:49:30 -0700

    fabricized

--- fabfile/setup.py
+++ fabfile/setup.py
@@ -5,14 +5,14 @@ from fabric.contrib.console import confirm
 @task
 def init():
 	# Touch README
-	local("rm README.md")
-	readme = prompt("Enter breif description for README: ").lower()
-	local("echo '{readme}' > README.md")
+	local('rm README.md')
+	readme = prompt('Enter breif description for README: ').lower()
+	local('echo "{}" > README.md'.format(readme))
 	
 	# Set Up App
 	app_name = prompt("Enter App Name: ").lower()
-	local("mkdir -p {app_name}/{api,resources,services,tasks}")
-	local("cd {app_name} && for DIR in $(find . -type d); do touch $DIR/__init__.py; done")
+	local("mkdir -p {}/{api,resources,services,tasks}".format(app_name))
+	local("cd {} && for DIR in $(find . -type d); do touch $DIR/__init__.py; done".format(app_name))
 	
 	# Set Up Static Media
 	media = prompt("Setup Static Media (Y/N): ")
@@ -26,9 +26,14 @@ def init():
 	# Setup Git
 	local("rm -rf .git/ && git init && git add .")
 	commit = prompt("Git Commit Message: ").lower()
-	local("git commit -am {commit}")
+	local("git commit -am {}".format(commit))
 	origin = prompt("Add Origin: ")
-	local("git remote add origin {origin} && git push -u origin master")
+	local("git remote add origin {} && git push -u origin master".format(origin))
+
+
+@task
+def run(target="run.py"):
+    local("python {}".format(target))
 
 '''
 TODO: Fabric task for api
